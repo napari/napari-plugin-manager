8159636bc97f462b51c9f6206d9e31c1c5d984d2
jaimergp <jaimergp@users.noreply.github.com>
2023-04-25 12:51:29 +0200

    add fixture by @Czaki

---
+++ napari_plugin_manager/_tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from qtpy.QtWidgets import QDialog, QInputDialog, QMessageBox
+
+
+@pytest.fixture(autouse=True)
+def _block_message_box(monkeypatch, request):
+    def raise_on_call(*_, **__):
+        raise RuntimeError("exec_ call")  # pragma: no cover
+
+    monkeypatch.setattr(QMessageBox, "exec_", raise_on_call)
+    monkeypatch.setattr(QMessageBox, "critical", raise_on_call)
+    monkeypatch.setattr(QMessageBox, "information", raise_on_call)
+    monkeypatch.setattr(QMessageBox, "question", raise_on_call)
+    monkeypatch.setattr(QMessageBox, "warning", raise_on_call)
+    monkeypatch.setattr(QInputDialog, "getText", raise_on_call)
+    if "enabledialog" not in request.keywords:
+        monkeypatch.setattr(QDialog, "exec_", raise_on_call)
