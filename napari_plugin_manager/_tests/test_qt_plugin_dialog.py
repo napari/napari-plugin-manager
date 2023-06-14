9040aec3cbcdf8204c375fb1739611b762eddfe1
jaimergp <jaimergp@users.noreply.github.com>
2023-06-14 15:07:01 +0200

    add reasons

--- napari_plugin_manager/_tests/test_qt_plugin_dialog.py
+++ napari_plugin_manager/_tests/test_qt_plugin_dialog.py
@@ -214,9 +214,7 @@ def test_filter_not_available_plugins(request, plugin_dialog):
     should be enabled without the tooltip warning.
     """
     if "no-constructor" in request.node.name:
-        # the plugin_dialog fixture has this id
-        # skip for 'constructor' variant
-        pytest.skip()
+        pytest.skip(reason="This test is only relevant for constructor-based installs")
     item = plugin_dialog.available_list.item(0)
     widget = plugin_dialog.available_list.itemWidget(item)
     if widget:
@@ -263,14 +261,12 @@ def test_visible_widgets(request, plugin_dialog):
     Test that the direct entry button and textbox are visible
     """
     if "no-constructor" not in request.node.name:
-        # the plugin_dialog fixture has this id
-        # skip for 'constructor' variant
-        pytest.skip()
+        pytest.skip(reason="Tested functionality not available in constructor-based installs")
     assert plugin_dialog.direct_entry_edit.isVisible()
     assert plugin_dialog.direct_entry_btn.isVisible()
 
 
-def test_version_dropdown(qtbot, plugin_dialog):
+def test_version_dropdown(plugin_dialog):
     """
     Test that when the source drop down is changed, it displays the other versions properly.
     """
