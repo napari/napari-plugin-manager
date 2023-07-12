ccd796e2c34429f966119d5f840d5b167a1e5e7e
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2023-07-12 11:47:54 -0500

    Move conda util to utils module and clean up code (#13)
    * Move conda util to utils module and clean up code
    
    * Fix drodown displays for non conda installations
    
    * Apply suggestions from code review
    
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>
    
    ---------
    
    Co-authored-by: Grzegorz Bokota <bokota+github@gmail.com>

---
+++ napari_plugin_manager/_tests/test_utils.py
@@ -0,0 +1,27 @@
+import sys
+from unittest.mock import patch
+
+import pytest
+
+from napari_plugin_manager.utils import is_conda_package
+
+
+@pytest.mark.parametrize(
+    "pkg_name,expected",
+    [
+        ("some-package", True),
+        ("some-other-package", False),
+        ("some-package-other", False),
+        ("other-some-package", False),
+        ("package", False),
+        ("some", False),
+    ],
+)
+def test_is_conda_package(pkg_name, expected, tmp_path):
+    mocked_conda_meta = tmp_path / 'conda-meta'
+    mocked_conda_meta.mkdir()
+    mocked_package = mocked_conda_meta / 'some-package-0.1.1-0.json'
+    mocked_package.touch()
+
+    with patch.object(sys, 'prefix', tmp_path):
+        assert is_conda_package(pkg_name) is expected
---
+++ napari_plugin_manager/utils.py
@@ -0,0 +1,21 @@
+import re
+import sys
+from pathlib import Path
+
+
+def is_conda_package(pkg: str) -> bool:
+    """Determines if plugin was installed through conda.
+
+    Returns
+    -------
+    bool
+        ``True` if a conda package, ``False`` if not.
+    """
+    # Installed conda packages within a conda installation and environment can
+    # be identified as files with the template ``<package-name>-<version>-<build-string>.json``
+    # saved within a ``conda-meta`` folder within the given environment of interest.
+    conda_meta_dir = Path(sys.prefix) / 'conda-meta'
+    return any(
+        re.match(rf"{pkg}-[^-]+-[^-]+.json", p.name)
+        for p in conda_meta_dir.glob(f"{pkg}-*-*.json")
+    )
