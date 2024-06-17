9b0e09331f1a6f0b6523b863152618b2e1a593c0
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2024-06-17 14:17:33 -0500

    Test togle status

--- napari_plugin_manager/_tests/test_qt_plugin_dialog.py
+++ napari_plugin_manager/_tests/test_qt_plugin_dialog.py
@@ -377,3 +377,10 @@ def test_add_items_outdated(plugin_dialog):
     widget = plugin_dialog.installed_list.itemWidget(item)
 
     assert widget.update_btn.isVisible()
+
+
+def test_toggle_status(plugin_dialog):
+    plugin_dialog.toggle_status(True)
+    assert plugin_dialog.stdout_text.isVisible()
+    plugin_dialog.toggle_status(False)
+    assert not plugin_dialog.stdout_text.isVisible()
