f577d1e75cda4fc2d3ee95beb089c6ecd012cbe5
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2024-08-08 22:52:37 -0500

    Style button

--- napari_plugin_manager/qt_plugin_dialog.py
+++ napari_plugin_manager/qt_plugin_dialog.py
@@ -1193,30 +1193,27 @@ class QtPluginDialog(QDialog):
         self.direct_entry_edit.returnPressed.connect(self._install_packages)
         self.direct_entry_edit.setVisible(visibility_direct_entry)
         self.direct_entry_btn = QToolButton(self)
-        self.direct_entry_btn.setIcon(
-            QColoredSVGIcon.from_resources('logo_silhouette')
-        )
-        # self.direct_entry_btn = QPushButton(trans._("Install"), self)
         self.direct_entry_btn.setVisible(visibility_direct_entry)
         self.direct_entry_btn.clicked.connect(self._install_packages)
         self.direct_entry_btn.setText(trans._("Install"))
         self.direct_entry_btn.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
 
-        if True:
-            # if IS_NAPARI_CONDA_INSTALLED:
-            self._menu = QMenu(self)
+        if IS_NAPARI_CONDA_INSTALLED:
             self._action_conda = QAction(trans._('Conda'), self)
             self._action_conda.setCheckable(True)
+            self._action_conda.setChecked(True)
+
             self._action_pypi = QAction(trans._('PyPI'), self)
             self._action_pypi.setCheckable(True)
-            self._action_conda.setChecked(True)
-            self._menu.addAction(self._action_conda)
-            self._menu.addAction(self._action_pypi)
 
             self._action_group = QActionGroup(self)
             self._action_group.addAction(self._action_pypi)
             self._action_group.addAction(self._action_conda)
             self._action_group.setExclusive(True)
+
+            self._menu = QMenu(self)
+            self._menu.addAction(self._action_conda)
+            self._menu.addAction(self._action_pypi)
 
             self.direct_entry_btn.setMenu(self._menu)
 
