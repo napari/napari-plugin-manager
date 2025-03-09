ca5e28ac95f6867a230739e230045506365cb846
Grzegorz Bokota <bokota+github@gmail.com>
2025-03-09 17:45:41 +0100

    pin all criticall pacakages in olugin installer

--- src/napari_plugin_manager/qt_package_installer.py
+++ src/napari_plugin_manager/qt_package_installer.py
@@ -11,18 +11,44 @@ import atexit
 import os
 import sys
 from functools import lru_cache
+from importlib.metadata import version as package_version
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import Sequence
 
-from napari._version import version as _napari_version
-from napari._version import version_tuple as _napari_version_tuple
+from packaging.version import parse as parse_version
 
 from napari_plugin_manager.base_qt_package_installer import (
     CondaInstallerTool,
     InstallerQueue,
     PipInstallerTool,
 )
+
+CRITICAL_PACKAGES = [
+    "app-model",
+    "lazy_loader",
+    "magicgui",
+    "napari",
+    "numpy",
+    "psygnal",
+    "pydantic",
+    "superqt",
+    "vispy",
+]
+
+CRITICAL_PACKAGES_PIN = [
+    f"{pkg}=={package_version(pkg)}" for pkg in CRITICAL_PACKAGES
+]
+
+# dev or rc versions might not be available in public channels
+# but only installed locally - if we try to pin those, mamba
+# will fail to pin it because there's no record of that version
+# in the remote index, only locally; to work around this bug
+# we will have to pin to e.g. 0.4.* instead of 0.4.17.* for now
+CRITICAL_PACKAGES_PIN_CONDA = [
+    f"{pkg}={parse_version(package_version(pkg)).base_version}"
+    for pkg in CRITICAL_PACKAGES
+]
 
 
 def _get_python_exe():
@@ -48,7 +74,7 @@ class NapariPipInstallerTool(PipInstallerTool):
         """
         Version constraints to limit unwanted changes in installation.
         """
-        return [f"napari=={_napari_version}", "numpy<2"]
+        return CRITICAL_PACKAGES_PIN
 
     @classmethod
     @lru_cache(maxsize=0)
@@ -64,18 +90,8 @@ class NapariPipInstallerTool(PipInstallerTool):
 class NapariCondaInstallerTool(CondaInstallerTool):
     @staticmethod
     def constraints() -> Sequence[str]:
-        # FIXME
-        # dev or rc versions might not be available in public channels
-        # but only installed locally - if we try to pin those, mamba
-        # will fail to pin it because there's no record of that version
-        # in the remote index, only locally; to work around this bug
-        # we will have to pin to e.g. 0.4.* instead of 0.4.17.* for now
-        version_lower = _napari_version.lower()
-        is_dev = "rc" in version_lower or "dev" in version_lower
-        pin_level = 2 if is_dev else 3
-        version = ".".join([str(x) for x in _napari_version_tuple[:pin_level]])
 
-        return [f"napari={version}", "numpy<2.0a0"]
+        return CRITICAL_PACKAGES_PIN_CONDA
 
 
 class NapariInstallerQueue(InstallerQueue):
